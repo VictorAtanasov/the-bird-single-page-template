754072e4422ce81bf5dbc786664b500cd863db2b
VictorAtanasov <viktor_atanasovv@yahoo.com>
2017-06-25 17:43:57 +0300

    Footer Hover Effects

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,7 +9,6 @@ $(document).scroll(function(){
     var logoTop = $('.logoTop');
     logoTop.attr('src', './css/Media/logo_sm_dark.png');
     
-    
     var menu = $('.menu');
     menu.addClass('sticky-header')
     if (scrollTop == 0){
@@ -17,3 +16,20 @@ $(document).scroll(function(){
         logoTop.attr('src', './css/Media/logo_sm.png');
     } 
 })
+
+var msgBtn = document.getElementById('btn');
+var inputName = document.getElementById('input-name');
+var inputEmail = document.getElementById('input-email');
+var inputMessage = document.getElementById('input-message');
+
+function validateInputs (inputName,inputEmail,inputMessage){
+    if (inputName.value == '' || inputEmail.value == '' || inputMessage.value == ''){
+        console.log('OPA')
+    }
+}
+
+msgBtn.addEventListener('click', function(){
+    validateInputs(inputName,inputEmail,inputMessage);
+    //console.log(inputMessage.value)
+});
+
